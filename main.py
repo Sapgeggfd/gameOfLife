554eb2e368392fa3a30da1c0f083f93031a0d3b1
Simon Geigenfeind <sgeigenfeind@cms-it.de>
2025-08-07 16:40:13 +0200

    optimized and added form from file read

--- main.py
+++ main.py
@@ -1,51 +1,53 @@
+import os
 import random
 import time
 
+type Field = list[list[bool]]
+
 # Vertical screen w,h
-size: tuple[int, int] = 176, 160
+# size: tuple[int, int] = 176, 160
 # Horizontal screen w,h
 # size: tuple[int, int] = 316, 86
 # Laptop screen w,h
-# size: tuple[int, int] = 236, 63
+size: tuple[int, int] = 236, 63
 
 
-filed: list[list[bool]] = [[False for _ in range(size[0])] for _ in range(size[1])]
-# filed[0][0] = True
+def get_clear_field() -> Field:
+    return [[False for _ in range(size[0])] for _ in range(size[1])]
 
 
-def gen_random() -> None:
+def gen_random() -> Field:
     size_square: int = (size[0] + size[1]) // 2
     offsets: int = size[0]
 
-    for _ in range(random.randint(size_square - offsets, size_square + offsets) * (size[0] // 10)):
-        filed[random.randint(size[1] // 2 - offsets, size[1] // 2 + offsets) % size[1]][
-            random.randint(size[0] // 2 - offsets, size[0] // 2 + offsets) % size[0]
-        ] = True
+    new_field: Field = get_clear_field()
+
+    l1: int = size[1] // 2 - offsets
+    h1: int = size[1] // 2 + offsets
+
+    l2: int = size[0] // 2 - offsets
+    h2: int = size[0] // 2 + offsets
+
+    for _ in range(random.randint(size_square - offsets, size_square + offsets) * (size[0] // 30)):
+        new_field[random.randint(l1, h1) % size[1]][random.randint(l2, h2) % size[0]] = True
+
+    return new_field
 
 
-# def is_empty() -> bool:
-#     for line in filed:
-#         for cell in line:
-#             if cell:
-#                 return False
-#     return True
-
-
-def cycle(filed: list[list[bool]]) -> tuple[list[list[bool]], int]:
+def cycle(filed: Field) -> tuple[Field, int]:
     actions = 0
-    next_filed: list[list[bool]] = [[False for _ in range(size[0])] for _ in range(size[1])]
+    next_filed: Field = get_clear_field()
     for y, line in enumerate(filed):
-        for x, _ in enumerate(line):
-            top: list[bool] = [filed[(y + 1) % size[1]][(x + x_off) % size[0]] for x_off in range(-1, 2)]
-            bottom: list[bool] = [filed[(y - 1) % size[1]][(x + x_off) % size[0]] for x_off in range(-1, 2)]
-
-            left: bool = filed[y][(x - 1) % size[0]]
-            right: bool = filed[y][(x + 1) % size[0]]
-
-            alive_neighbors: int = [*top, *bottom, left, right].count(True)
+        for x, cell in enumerate(line):
+            alive_neighbors: int = [
+                *[filed[(y + 1) % size[1]][(x + x_off) % size[0]] for x_off in range(-1, 2)],
+                *[filed[(y - 1) % size[1]][(x + x_off) % size[0]] for x_off in range(-1, 2)],
+                filed[y][(x - 1) % size[0]],
+                filed[y][(x + 1) % size[0]],
+            ].count(True)
 
             if alive_neighbors == 2:
-                next_filed[y][x] = filed[y][x]
+                next_filed[y][x] = cell
                 actions += 1
             elif alive_neighbors == 3:
                 next_filed[y][x] = True
@@ -55,34 +57,50 @@ def cycle(filed: list[list[bool]]) -> tuple[list[list[bool]], int]:
     return next_filed, actions
 
 
-gen_random()
+def read_form(path: str = "form.gol") -> Field:
+    with open(f"forms/{path}", "r") as file:
+        return [[True if c != " " else False for c in list(line.strip("\n"))] for line in file.readlines()]
+
+
+def a(filed: Field, figure: Field, offset: tuple[int, int]) -> Field:
+    for y, line in enumerate(figure):
+        for x, cell in enumerate(line):
+            filed[y + offset[1]][x + offset[0]] = cell
+
+    return filed
+
 
 steps = 0
 last_moves: list[int] = []
+filed: Field = get_clear_field()
+
+
+# filed = gen_random()
+form: Field = read_form(path="glider_gun.gol")
+
+filed = a(filed=filed, figure=form, offset=(0, 0))
 
 highest_step_count: int = 0
 try:
     while True:
         steps += 1
-        for line in filed:
-            for row in line:
-                print("+" if row else " ", end="", flush=False)
-            print(flush=False)
-        print(flush=True)
-        # time.sleep(0.1)
-
-        # input()
+        os.system(command="clear")
+        print("\n".join(["".join(["#" if cell else " " for cell in line]) for line in filed]))
+        if steps == 1:
+            input("Press Enter to start")
 
         filed, actions = cycle(filed=filed)
         last_moves.append(actions)
-        if steps > 100:
+
+        if steps % 100 and False:
             if last_moves.count(actions) > 50:
-                if highest_step_count < steps:
-                    highest_step_count = steps
+                highest_step_count = steps if highest_step_count < steps else highest_step_count
                 steps = 0
                 last_moves = []
-                gen_random()
+                filed: Field = get_clear_field()
+                filed = gen_random()
                 print(f"not enough action. Highest steps so far:{highest_step_count}")
                 time.sleep(1)
+
 except KeyboardInterrupt:
     print(f"{highest_step_count=}")
